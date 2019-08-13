28a908fc82d616021c60a4abcb5cfce8155b52b4
Kevin <kevin.liu@sifive.com>
2019-08-13 08:42:52 +0800

    move devece/TL/TestRAM.scala to lib-TLadapters-CA

--- library-tilelinkadapters-chipsalliance/src/main/scala/TestRAM.scala
+++ library-tilelinkadapters-chipsalliance/src/main/scala/TestRAM.scala
@@ -63,22 +63,4 @@ class TLTestRAM(address: AddressSet, executable: Boolean = true, beatBytes: Int 
   }
 }
 
-/** Synthesizeable unit testing */
-import freechips.rocketchip.unittest._
 
-class TLRAMZeroDelay(ramBeatBytes: Int, txns: Int)(implicit p: Parameters) extends LazyModule {
-  val fuzz = LazyModule(new TLFuzzer(txns))
-  val model = LazyModule(new TLRAMModel("ZeroDelay"))
-  val ram  = LazyModule(new TLTestRAM(AddressSet(0x0, 0x3ff), beatBytes = ramBeatBytes))
-
-  ram.node := TLDelayer(0.25) := model.node := fuzz.node
-
-  lazy val module = new LazyModuleImp(this) with UnitTestModule {
-    io.finished := fuzz.module.io.finished
-  }
-}
-
-class TLRAMZeroDelayTest(ramBeatBytes: Int, txns: Int = 5000, timeout: Int = 500000)(implicit p: Parameters) extends UnitTest(timeout) {
-  val dut = Module(LazyModule(new TLRAMZeroDelay(ramBeatBytes, txns)).module)
-  io.finished := dut.io.finished
-}
---
+++ library-tilelinkadaptersunittest-chipsalliance/src/main/scala/TestRAM.scala
@@ -0,0 +1,26 @@
+/** Synthesizeable unit testing */
+package freechips.rocketchip.devices.tilelink
+
+import Chisel._
+import freechips.rocketchip.config.Parameters
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.tilelink._
+import freechips.rocketchip.util._
+import freechips.rocketchip.unittest._
+
+class TLRAMZeroDelay(ramBeatBytes: Int, txns: Int)(implicit p: Parameters) extends LazyModule {
+  val fuzz = LazyModule(new TLFuzzer(txns))
+  val model = LazyModule(new TLRAMModel("ZeroDelay"))
+  val ram  = LazyModule(new TLTestRAM(AddressSet(0x0, 0x3ff), beatBytes = ramBeatBytes))
+
+  ram.node := TLDelayer(0.25) := model.node := fuzz.node
+
+  lazy val module = new LazyModuleImp(this) with UnitTestModule {
+    io.finished := fuzz.module.io.finished
+  }
+}
+
+class TLRAMZeroDelayTest(ramBeatBytes: Int, txns: Int = 5000, timeout: Int = 500000)(implicit p: Parameters) extends UnitTest(timeout) {
+  val dut = Module(LazyModule(new TLRAMZeroDelay(ramBeatBytes, txns)).module)
+  io.finished := dut.io.finished
+}
